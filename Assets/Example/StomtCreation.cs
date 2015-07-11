24b74ee57df3123122a109c5a2f4243d18cae6c0
Elias Steurer <i@kelteseth.net>
2015-07-11 15:53:14 +0200

    [Added] basic stomtCreation script and working button toggle

---
+++ Assets/Example/StomtCreation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StomtCreation : MonoBehaviour {
+
+    public GameObject btn_wish, btn_like, toggle;
+    public Text message;
+    
+
+
+	void Start()
+    {
+
+	}
+	
+
+	void Update()
+    {
+	
+	}
+
+    public void togglePressed()
+    {
+        Canvas wish = btn_wish.GetComponent<Canvas>();
+        Canvas like = btn_like.GetComponent<Canvas>();
+
+        if(wish.sortingOrder == 1)
+        {
+            like.sortingOrder = 1;
+            wish.sortingOrder = 2;
+        }
+        else
+        {
+            like.sortingOrder = 2;
+            wish.sortingOrder = 1;
+        }
+    }
+
+}
