d4df0a6dd8b99c7ec9973e6e377709aaa5eabf74
Crosire <crosiredev@gmail.com>
2015-07-11 16:34:25 +0200

    [Added] stomt API script for server communication

---
+++ Assets/StomtAPI.cs
@@ -0,0 +1,177 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public struct Stomt
+{
+	public struct Target
+	{
+		public string id { get; set; }
+		public string displayname { get; set; }
+	}
+
+	public string id { get; set; }
+	public bool negative { get; set; }
+	public string text { get; set; }
+	public string lang { get; set; }
+	public string created_at { get; set; }
+	public bool anonym { get; set; }
+	public Target creator { get; set; }
+}
+
+public class StomtAPI : MonoBehaviour
+{
+	[SerializeField]
+	string _AppId = "";
+	[SerializeField]
+	string _TargetName = "";
+	[SerializeField]
+	string _AccessToken = "";
+
+	public string AppId
+	{
+		get { return _AppId; }
+	}
+	public string TargetName
+	{
+		get { return _TargetName; }
+	}
+
+	void Start()
+	{
+		ServicePointManager.ServerCertificateValidationCallback += delegate { return true; };
+	}
+
+	public void RequestFeed(ref List<Stomt> outlist)
+	{
+		RequestFeed(_TargetName, ref outlist);
+	}
+	public void RequestFeed(string target, ref List<Stomt> outlist)
+	{
+		StartCoroutine(RequestFeedAsync(target, outlist));
+	}
+	IEnumerator RequestFeedAsync(string target, List<Stomt> outlist)
+	{
+		const int limit = 15;
+
+		HttpWebRequest request = HttpWebRequest.Create(string.Format("https://test.rest.stomt.com/targets/{0}/stomts/received?limit={1}", target, limit)) as HttpWebRequest;
+		request.Method = "GET";
+		request.ContentType = "application/json";
+		request.Headers["appid"] = _AppId;
+		request.Headers["accesstoken"] = _AccessToken;
+
+		var async1 = request.BeginGetResponse(null, null);
+
+		yield return async1;
+
+		HttpWebResponse response = null;
+		Stream responseStream = null;
+
+		try
+		{
+			response = request.EndGetResponse(async1) as HttpWebResponse;
+			responseStream = response.GetResponseStream();
+		}
+		catch (WebException ex)
+		{
+			Debug.LogException(ex);
+			yield break;
+		}
+
+		string dataText = string.Empty;
+		byte[] dataBuffer = new byte[2048];
+
+		while (true)
+		{
+			var async2 = responseStream.BeginRead(dataBuffer, 0, dataBuffer.Length, null, null);
+
+			yield return async2;
+
+			int length = responseStream.EndRead(async2);
+
+			if (length <= 0)
+			{
+				break;
+			}
+
+			dataText += Encoding.UTF8.GetString(dataBuffer, 0, length);
+		}
+
+		responseStream.Close();
+
+		LitJson.JsonData data = LitJson.JsonMapper.ToObject(dataText);
+
+		if (data.Keys.Contains("error"))
+		{
+			Debug.LogError((string)data["error"]["msg"]);
+			yield break;
+		}
+		else
+		{
+			data = data["data"];
+		}
+
+		for (int i = 0; i < data.Count; i++)
+		{
+			outlist.Add(LitJson.JsonMapper.ToObject<Stomt>(data[i].ToJson()));
+		}
+	}
+
+	public void CreateStomt(bool negative, string text)
+	{
+		CreateStomt(negative, _TargetName, text);
+	}
+	public void CreateStomt(bool negative, string target, string text)
+	{
+		StringBuilder json = new StringBuilder();
+		LitJson.JsonWriter writer = new LitJson.JsonWriter(json);
+
+		writer.WriteObjectStart();
+		writer.WritePropertyName("anonym");
+		writer.Write(true);
+		writer.WritePropertyName("negative");
+		writer.Write(negative);
+		writer.WritePropertyName("target_id");
+		writer.Write(target);
+		writer.WritePropertyName("text");
+		writer.Write(text);
+		writer.WritePropertyName("creator");
+		writer.WriteObjectStart();
+		writer.WritePropertyName("id");
+		writer.Write("unity");
+		writer.WriteObjectEnd();
+		writer.WriteObjectEnd();
+
+		StartCoroutine(CreateStomtAsync(json.ToString()));
+	}
+	IEnumerator CreateStomtAsync(string json)
+	{
+		byte[] data = Encoding.UTF8.GetBytes(json);
+
+		HttpWebRequest request = HttpWebRequest.Create("https://test.rest.stomt.com/stomts") as HttpWebRequest;
+		request.Method = "POST";
+		request.ContentType = "application/json";
+		request.ContentLength = data.Length;
+		request.Headers["appid"] = _AppId;
+		request.Headers["accesstoken"] = _AccessToken;
+
+		var async1 = request.BeginGetRequestStream(null, null);
+
+		yield return async1;
+
+		try
+		{
+			Stream requestStream = request.EndGetRequestStream(async1);
+			requestStream.Write(data, 0, data.Length);
+			requestStream.Close();
+		}
+		catch (WebException ex)
+		{
+			Debug.LogException(ex);
+			yield break;
+		}
+	}
+}
