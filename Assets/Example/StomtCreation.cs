234dac98125035a5461b1c78ca425ce101d26d57
Crosire <crosiredev@gmail.com>
2015-08-09 16:08:02 +0200

    [Fixed] stomt creation character limit

--- Assets/Example/StomtCreation.cs
+++ Assets/Example/StomtCreation.cs
@@ -35,14 +35,25 @@ public class StomtCreation : MonoBehaviour
 			_Wish.sortingOrder = 1;
 			_WouldBecauseText.text = "because";
 		}
+
+		OnMessageChanged();
 	}
 	public void OnMessageChanged()
 	{
-		_CharacterLimit.text = (129 - _Message.text.Length).ToString();
+		int limit = 101 - _WouldBecauseText.text.Length;
+		int reverselength = limit - _Message.text.Length;
+
+		if (reverselength <= 0)
+		{
+			reverselength = 0;
+			_Message.text = _Message.text.Substring(0, limit);
+		}
+
+		_CharacterLimit.text = reverselength.ToString();
 	}
 	public void OnPostButtonPressed()
 	{
-		if (_Message.text.Length <= 10)
+		if (_Message.text.Length == 0)
 		{
 			return;
 		}
