976208640e293ccc99e67db28b80b3112a533253
DANI <daniel.schukies@gmail.com>
2017-09-30 13:27:59 +0200

    Add workaround for "Invalid certificate received from server" Problem could not be reproduced (fix not tested)

--- Scripts/StomtAPI.cs
+++ Scripts/StomtAPI.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Net;
 using System.Text;
 using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace Stomt
 {
@@ -231,6 +233,9 @@ namespace Stomt
 		{
 			HttpWebRequest request = WebRequest("GET", string.Format("{0}/targets/{1}", restServerURL, target));
 
+            // Workaround for certificate problem
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
+
 			// Send request and wait for response
 			var async1 = request.BeginGetResponse(null, null);
 
@@ -293,6 +298,9 @@ namespace Stomt
 		IEnumerator LoadFeedAsync(string target, FeedCallback callback, int offset, int limit)
 		{
 			HttpWebRequest request = WebRequest("GET", string.Format("{0}/targets/{1}/stomts/received?offset={2}&limit={3}", restServerURL, target, offset, limit));
+
+            // Workaround for certificate problem
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
 
 			// Send request and wait for response
 			var async1 = request.BeginGetResponse(null, null);
@@ -380,6 +388,9 @@ namespace Stomt
             HttpWebRequest request = WebRequest("POST", string.Format("{0}/stomts", restServerURL));
 			request.ContentLength = data.Length;
 
+            // Workaround for certificate problem
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
+
 			// Send request
 			var async1 = request.BeginGetRequestStream(null, null);
 
@@ -400,6 +411,9 @@ namespace Stomt
 				Debug.LogException(ex);
 				yield break;
 			}
+
+            // Workaround for certificate problem
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
 
 			// Wait for response
 			var async2 = request.BeginGetResponse(null, null);
@@ -432,6 +446,9 @@ namespace Stomt
             HttpWebRequest request = WebRequest("POST", string.Format("{0}/images", restServerURL));
 			request.ContentLength = data.Length;
 
+            // Workaround for certificate problem
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
+
 			// Send request
 			var async1 = request.BeginGetRequestStream(null, null);
 
@@ -452,6 +469,9 @@ namespace Stomt
 				Debug.LogException(ex);
 				yield break;
 			}
+
+            // Workaround for certificate problem
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
 
 			// Wait for response
 			var async2 = request.BeginGetResponse(null, null);
@@ -530,6 +550,31 @@ namespace Stomt
             {
                 return null;
             }
+        }
+
+        public bool RemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            bool isOk = true;
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if (sslPolicyErrors != SslPolicyErrors.None)
+            {
+                for (int i = 0; i < chain.ChainStatus.Length; i++)
+                {
+                    if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                    {
+                        chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                        chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                        chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                        chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                        bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                        if (!chainIsValid)
+                        {
+                            isOk = false;
+                        }
+                    }
+                }
+            }
+            return isOk;
         }
 	}
 }
