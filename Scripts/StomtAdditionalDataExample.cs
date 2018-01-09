c24f9aaaf865c2e52b8fc080e39aa1c3b39e86aa
DANI <daniel.schukies@gmail.com>
2018-01-09 20:17:56 +0100

    Add AdditionalDataExample

--- Scripts/StomtAdditionalDataExample.cs
+++ Scripts/StomtAdditionalDataExample.cs
@@ -4,15 +4,11 @@ using Stomt;
 
 public class StomtAdditionalDataExample : MonoBehaviour 
 {
-	private StomtAPI StomtAPI;
-
 	// Use this for initialization
 	void Start () 
 	{
-		StomtAPI = GameObject.Find("StomtPopup").GetComponent<StomtAPI>();
+		StomtAPI api = GameObject.Find("StomtPopup").GetComponent<StomtAPI>();
 
-		StomtAPI.AddCustomKeyValuePair("customkey", "blasasasasTEXT");
-
-		Debug.Log("TEST");
+		api.AddCustomKeyValuePair("CustomKey", "Value/Data");
 	}
 }
