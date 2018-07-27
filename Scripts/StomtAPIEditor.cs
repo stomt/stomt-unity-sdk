bca8f6fd13932f8749cbdcb3e3f58a870c5688a2
Max Klenk <max@klenk.biz>
2018-07-27 16:18:31 +0200

    feat(offline): save failed requsts offline and replay them later

---
+++ Scripts/StomtAPIEditor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace Stomt
+{
+	[CustomEditor(typeof(StomtAPI))]
+	public class StomtAPIEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			StomtAPI myScript = (StomtAPI)target;
+			if (GUILayout.Button("Delete local Session"))
+			{
+				myScript.cleanConfig();
+			}
+		}
+	}
+}
--- Scripts/StomtConfig.cs
+++ Scripts/StomtConfig.cs
@@ -5,34 +5,171 @@ using UnityEngine;
 
 namespace Stomt
 {
-	public class StomtConfig
+	public static class StomtConfig
 	{
 		public delegate void StomtConfigAction();
 		public static event StomtConfigAction OnStomtConfigUpdate;
-		private string StomtAccesstokenKey = "StomtAccesstoken";
-		private string StomtSubscribedKey = "StomtSubscribed";
-		private string StomtLoggedinKey = "StomtLoggedin";
-		private string Accesstoken;
-		private bool Subscribed;
-		private bool Loggedin;
 
-		public void Load()
+		private static string StomtAccesstokenKey = "StomtAccesstoken";
+		private static string StomtSubscribedKey = "StomtSubscribed";
+		private static string StomtLoggedinKey = "StomtLoggedin";
+		private static string StomtTargetIDKey = "StomtTargetID";
+		private static string StomtTargetDisplaynameKey = "StomtTargetDisplayname";
+		private static string StomtTargetImageUrlKey = "StomtTargetImageUrl";
+		private static string StomtTargetAmountStomtsKey = "StomtTargetAmountStomts";
+		private static string StomtUserIDKey = "StomtUserID";
+		private static string StomtUserDisplaynameKey = "StomtUserDisplayname";
+		private static string StomtUserImageUrlKey = "StomtUserImageUrl";
+		private static string StomtUserAmountStomtsKey = "StomtUserAmountStomts";
+
+		public static string AccessToken
 		{
-			this.Accesstoken = this.GetAccessToken();
-			this.Subscribed = this.GetSubscribed();
-			this.Loggedin = this.GetLoggedin();
-
-			if (OnStomtConfigUpdate != null)
-			{
-				OnStomtConfigUpdate();
+			get {
+				return PlayerPrefs.GetString(StomtConfig.StomtAccesstokenKey, "");
+			}
+			set {
+				StomtConfig.SetString(StomtConfig.StomtAccesstokenKey, value);
 			}
 		}
 
-		public void Delete()
+		public static bool Subscribed
 		{
-			PlayerPrefs.DeleteKey(this.StomtAccesstokenKey);
-			PlayerPrefs.DeleteKey(this.StomtLoggedinKey);
-			PlayerPrefs.DeleteKey(this.StomtSubscribedKey);
+			get
+			{
+				return Convert.ToBoolean(PlayerPrefs.GetString(StomtConfig.StomtSubscribedKey, "False"));
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtSubscribedKey, value.ToString());
+			}
+		}
+
+		public static bool LoggedIn
+		{
+			get
+			{
+				return Convert.ToBoolean(PlayerPrefs.GetString(StomtConfig.StomtLoggedinKey, "False"));
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtLoggedinKey, value.ToString());
+			}
+		}
+
+		public static string TargetID
+		{
+			get
+			{
+				return PlayerPrefs.GetString(StomtConfig.StomtTargetIDKey, "");
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtTargetIDKey, value);
+			}
+		}
+
+		public static string TargetDisplayname
+		{
+			get
+			{
+				return PlayerPrefs.GetString(StomtConfig.StomtTargetDisplaynameKey, "this game");
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtTargetDisplaynameKey, value);
+			}
+		}
+
+		public static string TargetImageUrl
+		{
+			get
+			{
+				return PlayerPrefs.GetString(StomtConfig.StomtTargetImageUrlKey, "");
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtTargetImageUrlKey, value);
+			}
+		}
+
+		public static int TargetAmountStomts
+		{
+			get
+			{
+				return PlayerPrefs.GetInt(StomtConfig.StomtTargetAmountStomtsKey, 0);
+			}
+			set
+			{
+				StomtConfig.SetInt(StomtConfig.StomtTargetAmountStomtsKey, value);
+			}
+		}
+
+		public static string UserID
+		{
+			get
+			{
+				return PlayerPrefs.GetString(StomtConfig.StomtUserIDKey, "");
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtUserIDKey, value);
+			}
+		}
+
+		public static string UserDisplayname
+		{
+			get
+			{
+				return PlayerPrefs.GetString(StomtConfig.StomtUserDisplaynameKey, "you");
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtUserDisplaynameKey, value);
+			}
+		}
+
+		public static string UserImageUrl
+		{
+			get
+			{
+				return PlayerPrefs.GetString(StomtConfig.StomtUserImageUrlKey, "");
+			}
+			set
+			{
+				StomtConfig.SetString(StomtConfig.StomtUserImageUrlKey, value);
+			}
+		}
+
+		public static int UserAmountStomts
+		{
+			get
+			{
+				return PlayerPrefs.GetInt(StomtConfig.StomtUserAmountStomtsKey, 0);
+			}
+			set
+			{
+				StomtConfig.SetInt(StomtConfig.StomtUserAmountStomtsKey, value);
+			}
+		}
+
+		//// Helpers
+
+		public static void Delete()
+		{
+			PlayerPrefs.DeleteKey(StomtConfig.StomtAccesstokenKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtSubscribedKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtLoggedinKey);
+
+			PlayerPrefs.DeleteKey(StomtConfig.StomtTargetIDKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtTargetDisplaynameKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtTargetImageUrlKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtTargetAmountStomtsKey);
+
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserIDKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserDisplaynameKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserImageUrlKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserAmountStomtsKey);
+
 			PlayerPrefs.Save();
 
 			if (OnStomtConfigUpdate != null)
@@ -41,139 +178,58 @@ namespace Stomt
 			}
 		}
 
-		public void SetAccessToken(string accesstoken)
+		public static void DeleteUser()
 		{
-			if (PlayerPrefs.HasKey(this.StomtAccesstokenKey))
-			{
-				if (PlayerPrefs.GetString(this.StomtAccesstokenKey).Equals(accesstoken))
-				{
-					return;
-				}
-			}
+			PlayerPrefs.DeleteKey(StomtConfig.StomtAccesstokenKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtSubscribedKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtLoggedinKey);
 
-			if (accesstoken != null)
-			{
-				this.Accesstoken = accesstoken;
-				PlayerPrefs.SetString(this.StomtAccesstokenKey, this.Accesstoken);
-				PlayerPrefs.Save();
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserIDKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserDisplaynameKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserImageUrlKey);
+			PlayerPrefs.DeleteKey(StomtConfig.StomtUserAmountStomtsKey);
+			PlayerPrefs.Save();
 
-				if (OnStomtConfigUpdate != null)
-				{
-					OnStomtConfigUpdate();
-				}
+			if (OnStomtConfigUpdate != null)
+			{
+				OnStomtConfigUpdate();
 			}
 		}
 
-		public void SetSubscribed(bool subscribed)
+		private static bool SetString(string key, string value)
 		{
-			if (PlayerPrefs.HasKey(this.StomtSubscribedKey))
+			if (PlayerPrefs.HasKey(key) && PlayerPrefs.GetString(key).Equals(value))
 			{
-				if (PlayerPrefs.GetString(this.StomtSubscribedKey).Equals(subscribed.ToString()) )
-				{
-					return;
-				}
+				return false;
 			}
 
-			if (!string.IsNullOrEmpty(subscribed.ToString()))
-			{
-				this.Subscribed = subscribed;
-				PlayerPrefs.SetString(this.StomtSubscribedKey, this.Subscribed.ToString());
-				PlayerPrefs.Save();
+			PlayerPrefs.SetString(key, value);
+			PlayerPrefs.Save();
 
-				if (OnStomtConfigUpdate != null)
-				{
-					OnStomtConfigUpdate();
-				}
+			if (OnStomtConfigUpdate != null)
+			{
+				OnStomtConfigUpdate();
 			}
+
+			return true;
 		}
 
-		public void SetLoggedin(bool loggedin)
+		private static bool SetInt(string key, int value)
 		{
-			if (PlayerPrefs.HasKey(this.StomtLoggedinKey))
+			if (PlayerPrefs.HasKey(key) && PlayerPrefs.GetInt(key) == value)
 			{
-				if (PlayerPrefs.GetString(this.StomtLoggedinKey).Equals(loggedin.ToString()))
-				{
-					return;
-				}
+				return false;
 			}
 
-			if (!string.IsNullOrEmpty(loggedin.ToString()))
-			{
-				this.Loggedin = loggedin;
-				PlayerPrefs.SetString(this.StomtLoggedinKey, this.Loggedin.ToString());
-				PlayerPrefs.Save();
+			PlayerPrefs.SetInt(key, value);
+			PlayerPrefs.Save();
 
-				if (OnStomtConfigUpdate != null)
-				{
-					OnStomtConfigUpdate();
-				}
-			}
-		}
-
-		public string GetAccessToken()
-		{
-			if (PlayerPrefs.HasKey(this.StomtAccesstokenKey))
+			if (OnStomtConfigUpdate != null)
 			{
-				if (PlayerPrefs.GetString(this.StomtAccesstokenKey).Equals(this.Accesstoken))
-				{
-					if (!string.IsNullOrEmpty(this.Accesstoken))
-					{
-						return this.Accesstoken;
-					}
-					else
-					{
-						//Debug.Log("Accesstoken was NullOrEmpty and Key was set");
-						return "";
-					}
-				}
-				else
-				{
-					return PlayerPrefs.GetString(this.StomtAccesstokenKey);
-				}
+				OnStomtConfigUpdate();
 			}
 
-			if (!string.IsNullOrEmpty(this.Accesstoken))
-			{
-				return this.Accesstoken;
-			}
-			else
-			{
-				return "";
-			}
-		}
-
-		public bool GetSubscribed()
-		{
-			if (PlayerPrefs.HasKey(this.StomtSubscribedKey))
-			{
-				if (PlayerPrefs.GetString(this.StomtSubscribedKey).Equals(this.Subscribed.ToString()))
-				{
-					return this.Subscribed;
-				}
-				else
-				{
-					return Convert.ToBoolean(PlayerPrefs.GetString(this.StomtSubscribedKey));
-				}
-			}
-
-			return false;
-		}
-
-		public bool GetLoggedin()
-		{
-			if (PlayerPrefs.HasKey(this.StomtLoggedinKey))
-			{
-				if (PlayerPrefs.GetString(this.StomtLoggedinKey).Equals(this.Loggedin.ToString()))
-				{
-					return this.Loggedin;
-				}
-				else
-				{
-					return Convert.ToBoolean(PlayerPrefs.GetString(this.StomtLoggedinKey));
-				}
-			}
-
-			return false;
+			return true;
 		}
 	}
 }
