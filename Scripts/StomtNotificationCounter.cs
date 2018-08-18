a1d18245e8f50086b327c591bba46fe7f2eca74d
Max Klenk <max@klenk.biz>
2018-08-18 15:03:17 +0200

    Add NotificationCounter prefab

---
+++ Scripts/StomtNotificationCounter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Stomt
+{
+	public class StomtNotificationCounter : MonoBehaviour
+	{
+		[SerializeField]
+		[HideInInspector]
+		protected GameObject UI;
+		[SerializeField]
+		[HideInInspector]
+		protected Text NotificationCount;
+
+		// Use this for initialization
+		void Start()
+		{
+			this.NotificationCount.text = 0.ToString();
+			this.UI.SetActive(false);
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			var count = StomtConfig.UserAmountNotifications;
+			NotificationCount.text = count.ToString();
+			this.UI.SetActive(count > 0);
+		}
+	}
+}
